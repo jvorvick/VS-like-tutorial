d97db48e9199bdb62af5c41e3a93ead4591c67df
jvorvick <49791235+jvorvick@users.noreply.github.com>
2025-07-25 15:42:26 -0700

    clean up

--- code/main.py
+++ code/main.py
@@ -1,21 +1,6 @@
 from settings import *
 from player import Player
 
-# class Player(pygame.sprite.Sprite):
-#     def __init__(self, groups):
-#         super().__init__(groups)
-#         self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
-#         self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-#         self.direction = pygame.Vector2()
-#         self.speed = 300
-
-#     def update(self, dt):
-#         keys = pygame.key.get_pressed()
-#         self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-#         self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-#         self.direction = self.direction.normalize() if self.direction else self.direction
-#         self.rect.center += self.direction * self.speed * dt
-
 class Game:
     def __init__(self):
         # setup
@@ -54,34 +39,3 @@ class Game:
 if __name__ == '__main__':
     game = Game()
     game.run()
-
-    # # general setup
-    # pygame.init()
-    # display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    # pygame.display.set_caption('Vampire Savior')
-    # running = True
-    # clock = pygame.time.Clock()
-
-    # # import
-
-    # # sprites
-    # all_sprites = pygame.sprite.Group()
-
-    # player = Player(all_sprites)
-    
-    # while running:
-    #     dt = clock.tick() / 1000
-    #     # event loop
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT:
-    #             running = False
-        
-    #     # update
-    #     all_sprites.update(dt)
-
-    #     # draw the game
-    #     display_surface.fill('#31662f')
-    #     all_sprites.draw(display_surface)
-    #     pygame.display.update()
-
-    # pygame.quit()
