7faaa6d0cf55e46ae1af37419b3424153abb7136
jvorvick <49791235+jvorvick@users.noreply.github.com>
2025-08-19 11:59:09 -0700

    camera -- add player

--- code/main.py
+++ code/main.py
@@ -22,17 +22,23 @@ class Game:
         self.setup()
 
         # sprites
-        self.player = Player((500,300), self.all_sprites, self.collision_sprites)
+        
         
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
 
         for x, y, image in map.get_layer_by_name('Ground').tiles():
             Sprite((x * TILE_SIZE,y * TILE_SIZE), image, self.all_sprites)
+
         for obj in map.get_layer_by_name('Objects'):
             CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
         for obj in map.get_layer_by_name('Collisions'):
             CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+        for obj in map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites)
 
     def run(self):
         while self.running:
