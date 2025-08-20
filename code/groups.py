581d1a931da8396bc06c20035346cec1a5a40b9f
jvorvick <49791235+jvorvick@users.noreply.github.com>
2025-08-20 13:27:37 -0700

    camera -- ysort exercise my solution

--- code/groups.py
+++ code/groups.py
@@ -4,25 +4,24 @@ class AllSprites(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
-        self.offset = pygame.Vector2()
+        self.offset = pygame.Vector2()  
+
+    def draw(self, target_pos):
+       
+        self.offset.x = -(target_pos[0] - WINDOW_WIDTH / 2)
+        self.offset.y = -(target_pos[1] - WINDOW_HEIGHT / 2)
+        for sprite in self:
+            self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
 
     def ysort(self):
         ysort = []
         for sprite in self:
             ysort.append(sprite)
             self.remove(sprite)
-        def sortFunc(e):
-            return e.rect.y
-        # print(ysort)
-        ysort.sort(key=sortFunc)
+        ysort.sort(key=lambda sprite : sprite.rect.centery)
         for y in ysort:
             self.add(y)
 
-        
-
-    def draw(self, target_pos):
-        # self.ysort()
-        self.offset.x = -(target_pos[0] - WINDOW_WIDTH / 2)
-        self.offset.y = -(target_pos[1] - WINDOW_HEIGHT / 2)
-        for sprite in self:
-            self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
+class GroundTiles(AllSprites):
+    def __init__(self):
+        super().__init__()
--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ from settings import *
 from player import Player
 from sprites import *
 from pytmx.util_pygame import load_pygame
-from groups import AllSprites
+from groups import AllSprites, GroundTiles
 
 from random import randint
 
@@ -18,43 +18,19 @@ class Game:
         # groups
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
-
+        self.ground_tiles = GroundTiles()
         self.setup()
         # sprites
         
         
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
-        objects_sort = []
-        # entities_sort = []
 
         for x, y, image in map.get_layer_by_name('Ground').tiles():
-            Sprite((x * TILE_SIZE,y * TILE_SIZE), image, self.all_sprites)
+            Sprite((x * TILE_SIZE,y * TILE_SIZE), image, self.ground_tiles)
 
         for obj in map.get_layer_by_name('Collisions'):
             CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
-
-        # for obj in map.get_layer_by_name('Objects'):
-        #     objects_sort.append(obj)
-            
-            
-        # for obj in map.get_layer_by_name('Entities'):
-        #     objects_sort.append(obj)
-
-        # def sortFunc(e):
-        #     return e.y
-
-        # objects_sort.sort(key=sortFunc)
-        
-        # for obj in objects_sort:
-        #     print(obj.name)
-        #     if obj.name == 'Player':
-        #         print(obj.y)
-        #         self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites)
-        #     elif not obj.name:
-        #         print(obj.y)
-        #         CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
-             
 
         for obj in map.get_layer_by_name('Objects'):
             # print(obj.y)
@@ -64,12 +40,6 @@ class Game:
             # print(obj.y)
             if obj.name == 'Player':
                 self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites)
-
-
-        
-
-
-        
 
     def run(self):
         while self.running:
@@ -82,18 +52,14 @@ class Game:
                     self.running = False
 
             # update
-            # for sprite in self.all_sprites:
-            #     print(sprite.rect.y)
-            # print(self.all_sprites)
-            
+            self.all_sprites.ysort()
+            self.ground_tiles.update()
             self.all_sprites.update(dt)
-            
 
             # draw
             self.display_surface.fill('black')
-            self.all_sprites.ysort()
-            # for sprite in self.all_sprites.sprites():
-            #     print('SPRITE', sprite.rect)
+
+            self.ground_tiles.draw(self.player.rect.center)
             self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
