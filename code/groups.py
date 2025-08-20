c259eff5c7abf0dbc5876d3e85070fdbaf23e30b
jvorvick <49791235+jvorvick@users.noreply.github.com>
2025-08-19 17:41:44 -0700

    camera -- ysort exercise wip

--- code/groups.py
+++ code/groups.py
@@ -6,7 +6,22 @@ class AllSprites(pygame.sprite.Group):
         self.display_surface = pygame.display.get_surface()
         self.offset = pygame.Vector2()
 
+    def ysort(self):
+        ysort = []
+        for sprite in self:
+            ysort.append(sprite)
+            self.remove(sprite)
+        def sortFunc(e):
+            return e.rect.y
+        # print(ysort)
+        ysort.sort(key=sortFunc)
+        for y in ysort:
+            self.add(y)
+
+        
+
     def draw(self, target_pos):
+        # self.ysort()
         self.offset.x = -(target_pos[0] - WINDOW_WIDTH / 2)
         self.offset.y = -(target_pos[1] - WINDOW_HEIGHT / 2)
         for sprite in self:
--- code/main.py
+++ code/main.py
@@ -20,25 +20,56 @@ class Game:
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
-
         # sprites
         
         
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
+        objects_sort = []
+        # entities_sort = []
 
         for x, y, image in map.get_layer_by_name('Ground').tiles():
             Sprite((x * TILE_SIZE,y * TILE_SIZE), image, self.all_sprites)
 
-        for obj in map.get_layer_by_name('Objects'):
-            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
-
         for obj in map.get_layer_by_name('Collisions'):
             CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
 
+        # for obj in map.get_layer_by_name('Objects'):
+        #     objects_sort.append(obj)
+            
+            
+        # for obj in map.get_layer_by_name('Entities'):
+        #     objects_sort.append(obj)
+
+        # def sortFunc(e):
+        #     return e.y
+
+        # objects_sort.sort(key=sortFunc)
+        
+        # for obj in objects_sort:
+        #     print(obj.name)
+        #     if obj.name == 'Player':
+        #         print(obj.y)
+        #         self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites)
+        #     elif not obj.name:
+        #         print(obj.y)
+        #         CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+             
+
+        for obj in map.get_layer_by_name('Objects'):
+            # print(obj.y)
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
         for obj in map.get_layer_by_name('Entities'):
+            # print(obj.y)
             if obj.name == 'Player':
                 self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites)
+
+
+        
+
+
+        
 
     def run(self):
         while self.running:
@@ -51,10 +82,18 @@ class Game:
                     self.running = False
 
             # update
+            # for sprite in self.all_sprites:
+            #     print(sprite.rect.y)
+            # print(self.all_sprites)
+            
             self.all_sprites.update(dt)
+            
 
             # draw
             self.display_surface.fill('black')
+            self.all_sprites.ysort()
+            # for sprite in self.all_sprites.sprites():
+            #     print('SPRITE', sprite.rect)
             self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
